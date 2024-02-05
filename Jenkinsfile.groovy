56928ab6b5f447a182c3f459fbb6ee9a80a9fc10
Jarry Jihed <jarryjihed@yahoo.fr>
2024-02-05 14:36:36 +0100

    sssb

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -69,7 +69,7 @@ pipeline{
 			agent any
                         steps{
                                 script {
-                                	withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'myregistry_login', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+                                	withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: registryCredential, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
       						sh 'curl -sk --user $USERNAME:$PASSWORD https://192.168.1.38:5000/v2/helloworld/tags/list'
       					}
                                 }
